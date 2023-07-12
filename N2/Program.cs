40829a411a086b666e77823fed1c4a831ece7371
Max <sultonbek.rakhimov@gmail.com>
2023-07-12 21:18:34 +0500

    Finished class N2

--- N2/Program.cs
+++ N2/Program.cs
@@ -1,4 +1,6 @@
 ﻿// Primitive types
+using System.Runtime.InteropServices;
+
 bool hasMoney = true;
 
 char dollarSign = '$';
@@ -103,12 +105,136 @@ DateTime now = DateTime.Now;
 Console.WriteLine(now);
 
 // Value and Reference types
+var isWhite = false;
+var address = "Bobur Shox ko'chasi, 8";
 
+// Static va Dynamic
 
+// Static
+var modelNumber = "34567890";
 
+// Dynamic
 
+// object bilan Member lariga murojaat qilib bo'maydi
+object exampleObject = new User();
+//exampleObject.Name = "Tom";
 
+// Dynamic tipga xohlagan tipdagi qiymatni saqlash mumkin
+object objectA = 10;
+objectA = "firstName";
+var unboxedString = (string)objectA;
 
+// Primtiv tiplar ishlatilganda dynamic tiplar bilan boxing-unboxing sodir bo'ladi
+// When used with primitive types, boxing-unboxing happens
+Console.WriteLine(unboxedString);
 
+// Can access members with dynamic
+// Dynamic tipi bilan memberlariga murojaat qilish mumkin
+dynamic objectB = new User();
+objectB.Name = "Bob";
+Console.WriteLine(objectB.Name);
+//Console.WriteLine(objectB.LastName);
 
+// Does change the address
+// Agar o'zgaruvchi HEAP da joylashgan bo'lsa dynamic ga o'tkazilsa,
+//  address o'zgarmaydi 
+//var staticString = "string";
+//object dynamicString = staticString;
+//string unboxedStringB = (string)dynamicString;
 
+//GCHandle handleA = GCHandle.Alloc(staticString, GCHandleType.Pinned);
+//IntPtr pointerA = handleA.AddrOfPinnedObject();
+//Console.WriteLine(pointerA.ToString());
+
+//GCHandle handleB = GCHandle.Alloc(dynamicString, GCHandleType.Pinned);
+//IntPtr pointerB = handleB.AddrOfPinnedObject();
+//Console.WriteLine(pointerB.ToString());
+
+//GCHandle handleC = GCHandle.Alloc(unboxedStringB, GCHandleType.Pinned);
+//IntPtr pointerC = handleC.AddrOfPinnedObject();
+//Console.WriteLine(pointerC.ToString());
+
+// Built-in and Custom types
+var dateTime = new DateTime(2023, 7, 12);
+var foydalanuvchi = new User();
+
+// Arrays
+// Bir o'lchamli massiv
+// Explicit e'lon qilganda tipida son ko'rsatilmaydi
+int[] sonlarMassiviA = new int[] { 1, 2, 3 };
+
+// E'lon qilganda tipida limitini berish mumkin
+var sonlarMassiviC = new int[default];
+
+// E'lon qilganda limit bilan boshlang'ich elemtlari soni teng bo'lishi shart
+var sonlarMassiviB = new int[3] { 1, 2, 3 };
+
+Console.WriteLine(sonlarMassiviB[0]);
+
+// Massivda elementlar soni o'zgarmaydi va yo'q bo'lgan
+// elementlarni murojaat qila olmaymiz
+//sonlarMassiviB[3] = 40;
+//sonlarMassiviB[-3] = 40;
+
+// Massiv elementlaridan hammasi ishlatish majburiy emas
+sonlarMassiviB[0] = 10;
+//sonlarMassiviB[1] = 20;
+sonlarMassiviB[2] = 30;
+
+// Ikki o'lchamli massiv
+var markazdagiXonalarA = new int[4,8];
+var markazdagiXonalarB = new int[,]
+{
+    {1, 2, 3, 4, 5, 6, 7, 8},
+    {1, 2, 3, 4, 5, 6, 7, 8},
+    {1, 2, 3, 4, 5, 6, 7, 8},
+    {1, 2, 3, 4, 5, 6, 7, 8},
+};
+
+// Jagged massiv
+var markazdagiXonalarC = new int[][]
+{
+    new int[6],
+    new int[8],
+    new int[8],
+    new int[5],
+};
+
+var markazdagiXonalarD = new int[][]
+{
+    new int[] {1, 2, 3},
+    new int[] {1, 2,4 },
+    new int[] {1, 2, 3, 4,5 },
+    new int[] {1,2,3}
+};
+
+var multiplyTable = new int[6, 3];
+
+// 10 * 3
+// 10 * 4
+// 10 * 5
+multiplyTable[0, 0] = 10;
+multiplyTable[0, 1] = 3;
+
+multiplyTable[1, 0] = 10;
+multiplyTable[1, 1] = 4;
+
+multiplyTable[2, 0] = 10;
+multiplyTable[2, 1] = 5;
+
+// Hisoblash
+for(var index = 0; index < 3; index++)
+    multiplyTable[index, 2] = multiplyTable[index, 0] * multiplyTable[index, 1];
+
+for (var index = 0; index < 3; index++)
+{
+    for (var indexB = 0; indexB < 3; indexB++)
+        Console.Write(multiplyTable[index, indexB] + " ");
+
+    Console.WriteLine();
+}
+
+public class User
+{
+    public string Name { get; set; }
+}
